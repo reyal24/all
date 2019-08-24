65133196559af83fb2322ad8d03442043009ecb9
kurniawanzaty <43376401+kurniawanzaty@users.noreply.github.com>
2019-08-24 13:03:17 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+#DOGEClickBot NEW UPDATE by AN Officiall
+
+#Bagaimana cara menggunakan?
+1. Jangan lupa Install **Python 2+**
+
+2. Install
+
+   >`pip install -r requirements.txt`
+
+4. Jalankan command prompt
+
+   >`python doge.py phone_number`
+
+5. Wait
+
+6. GOOD LUCK & ENJOY YOUR DOGECOIN
+
+JANGAN LUPA LIKE, COMMENT, & SUBSCRIBE !
+
+JANGAN LUPA IZIN DULU KALO MAU SHARE DI YT KALIAN..
+
---
+++ list.sh
@@ -0,0 +1,44 @@
+echo " "
+echo "        Ok Kita Gaskan Mamang "| lolcat
+sleep 1
+
+python nuyul.py +6281292541464
+sleep 1
+python nuyul.py +6288975636780
+sleep 1
+python nuyul.py +6287876137316
+sleep 1
+python nuyul.py +16306725814
+sleep 1
+python nuyul.py +15672340397
+sleep 1
+python nuyul.py +16303603789
+sleep 1
+python nuyul.py +12674994445
+sleep 1
+python nuyul.py +17753006268
+sleep 1
+python nuyul.py +17752966499
+sleep 1
+python nuyul.py +17752981034
+sleep 1
+python nuyul.py +15152123304
+sleep 1
+python nuyul.py +15156747363
+sleep 1
+python nuyul.py +15038545304
+sleep 1
+python nuyul.py +15059009851
+sleep 1
+python nuyul.py +17078876389
+sleep 1
+break
+
+
+
+
+
+
+
+
+
